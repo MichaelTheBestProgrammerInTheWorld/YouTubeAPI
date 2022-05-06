f2738b21640c05515e689e26babff84a7709fb7a
MichaelTheBestProgrammerInTheWorld <michaelmagdy_93@yahoo.com>
2022-05-06 23:53:05 +0200

    Setup Video Fragment

--- app/src/main/java/com/michaelmagdy/youtubeapi/network/ApiServices.kt
+++ app/src/main/java/com/michaelmagdy/youtubeapi/network/ApiServices.kt
@@ -1,6 +1,7 @@
 package com.michaelmagdy.youtubeapi.network
 
 import com.michaelmagdy.youtubeapi.model.ChannelModel
+import com.michaelmagdy.youtubeapi.model.VideoYtModel
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -12,4 +13,11 @@ interface ApiServices {
         @Query("part") part: String,
         @Query("id") id: String
     ) : Call<ChannelModel>
+
+    @GET("search")
+    fun getVideo(
+        @Query("part") part: String,
+        @Query("channelId") channelId: String,
+        @Query("order") order: String
+    ) : Call<VideoYtModel>
 }
--- app/src/main/java/com/michaelmagdy/youtubeapi/ui/videos/VideosFragment.kt
+++ app/src/main/java/com/michaelmagdy/youtubeapi/ui/videos/VideosFragment.kt
@@ -8,28 +8,40 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.michaelmagdy.youtubeapi.R
+import com.michaelmagdy.youtubeapi.adapter.VideoAdapter
+import com.michaelmagdy.youtubeapi.databinding.FragmentVideosBinding
 
 class VideosFragment : Fragment() {
 
-    private lateinit var videosViewModel: VideosViewModel
+    private var _binding: FragmentVideosBinding? = null
+    private val binding get() = _binding!!
+    private var videoViewModel: VideosViewModel? = null
+    private val adapter = VideoAdapter()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.rvVideo.adapter = adapter
+        binding.rvVideo.layoutManager = LinearLayoutManager(requireContext())
+
+        videoViewModel?.video?.observe(viewLifecycleOwner, {
+            if (it != null && it.items.isNotEmpty()){
+                adapter.setData(it.items)
+            }
+        })
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        videosViewModel =
+    ): View {
+        videoViewModel =
             ViewModelProvider(this).get(VideosViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_videos, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        videosViewModel.channel.observe(viewLifecycleOwner, Observer {
-            if (it != null && it.items.isNotEmpty()){
-                it.items.forEach {  channel ->
-                    textView.text = channel.snippet.title
-                }
-            }
-        })
-        return root
+        _binding = FragmentVideosBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
